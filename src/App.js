2874daf3eac782911ddb053fb26947e0e549909f
Ankur Bose <bose.ankur007@gmail.com>
2019-08-01 23:34:15 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Auxiliary from './hoc/Auxiliary';
+import Auxiliary from './hoc/Auxiliary/Auxiliary';
 function App() {
   return (
             <Auxiliary>
