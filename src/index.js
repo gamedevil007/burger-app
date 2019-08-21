4d8814878c687c1b9a6a6c20b1c7ecfb04c6a6bf
Ankur Bose <Ankur.Bose@cognizant.com>
2019-08-21 18:49:04 +0530

    saga added

--- src/index.js
+++ src/index.js
@@ -5,22 +5,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose,combineReducers } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import thunk from 'redux-thunk';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import createSagaMiddleware from 'redux-saga';
+import { watchAuth, watchBurgerBuilder, watchOrder } from './store/sagas/index';
 
-const rootReducer=combineReducers({
-    burgerBuilder:burgerBuilderReducer,
-    order:orderReducer,
-    auth:authReducer
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer,
+    auth: authReducer
 });
+
+const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     rootReducer, composeEnhancers(
-        applyMiddleware(thunk)
+        applyMiddleware(thunk, sagaMiddleware)
     ));
+
+sagaMiddleware.run(watchAuth);
+sagaMiddleware.run(watchBurgerBuilder);
+sagaMiddleware.run(watchOrder);
 const app = (
     <Provider store={store}>
         <BrowserRouter>
--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -30,12 +30,7 @@ export const fetchIngredientsFailed=()=>{
 }
 
 export const initIngredients=()=>{
-    return dispatch=>{
-        axios.get('https://react-my-burger-4d4cc.firebaseio.com/ingredients.json')
-        .then(response=>{
-            dispatch(setIngredients(response.data));
-        }).catch(error=>{
-            dispatch(fetchIngredientsFailed());
-        });
-    }
+    return {
+        type:actionTypes.INIT_INGREDIENTS
+    };
 }
